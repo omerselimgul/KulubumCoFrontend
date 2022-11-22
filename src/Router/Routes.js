fa064c35969c21a94b1e444ecf57db52bfaaa2ab
cancancann <hikmetcanoyke@outlook.com>
2022-11-22 11:20:56 +0300

    settings layout

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -5,6 +5,7 @@ import Login from '../pages/Login/Login';
 import Register from '../pages/Register/Register';
 import HomeLayout from '../layouts/HomeLayout/HomeLayout';
 import paths from './paths';
+import DetailClub from '../pages/DetailClub/DetailClub';
 // import Register from "../Pages/register/register";
 const Router = () => {
   const routes = useRoutes([
@@ -30,6 +31,10 @@ const Router = () => {
       path: '*',
       element: <Login />,
     },
+    {
+      path:paths.home.detail,
+      element:<DetailClub/>
+    }
   ]);
   return routes;
 };
