53b04d05837c1957b848548a4ca55dec2e6d4355
cancancann <hikmetcanoyke@outlook.com>
2022-12-07 00:45:53 +0300

    connect clubs Page and Universities page apı cal

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const BASE_URL = 'http://localhost:4000/api/';
 
-const API = axios.create({ baseURL: BASE_URL });
+
+const API = axios.create({ baseURL: BASE_URL ,withCredentials:true});
 
 const api = {
   auth: {
