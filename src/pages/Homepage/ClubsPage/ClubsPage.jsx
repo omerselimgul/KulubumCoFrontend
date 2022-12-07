3f7d7ab761f794186fb75b8c613bc5655c808b48
kadir <develioglu306@gmail.com>
2022-12-08 01:02:11 +0300

    Review

--- src/pages/Homepage/ClubsPage/ClubsPage.jsx
+++ src/pages/Homepage/ClubsPage/ClubsPage.jsx
@@ -1,44 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import styles from './club.module.scss';
 import horsePhoto from '../../../asset/image_5.png';
-import DuyuruNav from '../../../components/DuyuruNav/DuyuruNav'
+import DuyuruNav from '../../../components/DuyuruNav/DuyuruNav';
 import Card from '../../../components/Card/Card';
 import axios from 'axios';
 
 const ClubsPage = () => {
-  const url = "http://localhost:4000/api/club"
+  const url = 'http://localhost:4000/api/club';
   // const url2 = "http://localhost:4000/api/club/1"
   //{params:{page:1,limit:1}} //HATA VERİYOR.
 
   const [clubs, setClubs] = useState([]);
 
   useEffect(() => {
-    axios.get(url).then((res) => { setClubs(res.data.data) }).catch((err) => { console.log(err.response.data.message) })
-  }, [])
+    axios
+      .get(url)
+      .then((res) => {
+        setClubs(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+      });
+  }, []);
   // console.log(clubs)
 
   return (
-    <main className={styles.clubs} >
-      <div className={styles.clubsTitle}>
-        <DuyuruNav text='Kulüpler' />
-      </div>
+    <main className={styles.clubs}>
+      <DuyuruNav text="Kulüpler" />
       <div className={styles.clubsContent}>
         {clubs.map((club) => (
           <Card key={club.ClubId}>
             <Card.Img photo={horsePhoto} />
             <Card.Body>
               <Card.Title>{club.UniversityName}</Card.Title>
-              <Card.Owner >
-                {club.ClubName}
-              </Card.Owner>
+              <Card.Owner>{club.ClubName}</Card.Owner>
             </Card.Body>
           </Card>
         ))}
-
-
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default ClubsPage
+export default ClubsPage;
--- src/pages/Settings/Clubs/Clubs.jsx
+++ src/pages/Settings/Clubs/Clubs.jsx
@@ -5,75 +5,81 @@ import { useState } from 'react';
 import Button from '../../../components/Button';
 import Modal from '../../../components/Modal/Modal';
 import UploadSvg from '../../../asset/Vector.svg';
+import Card from './../../../components/Card/Card';
+import api from './../../../api/index';
 
 const Clubs = () => {
-  // const [myClubs, setMyClubs] = useState([]);
+  const [myClubs, setMyClubs] = useState([]);
   const [open, setOpen] = useState(false);
 
   return (
     <main className={styles.main}>
-
       <Modal className={styles.modal} open={open} setOpen={setOpen}>
         {/* CONTAİNER */}
         <div className={styles.modalContainer}>
           {/* RESİM DİV */}
           <div className={styles.modalImg}>
-            <img src={UploadSvg} alt='kadir' />
+            <img src={UploadSvg} alt="kadir" />
             <p>
               Resiminizi bu alana sürükleyin veya seçmek için
               <b> tıklayın.</b>
             </p>
           </div>
           {/* İNPUT */}
-          <SettingsInput className={styles.titleInput} label='Duyuru Başlığı' placeholder='Kulüp Başlığını giriniz' />
+          <SettingsInput className={styles.titleInput} label="Duyuru Başlığı" placeholder="Kulüp Başlığını giriniz" />
           {/* DUYURU METNİ */}
           <div className={styles.modalTextarea}>
-            <textarea value="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry' s standard dummyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+            <textarea
+              value="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry' s standard dummyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
             dummyLorem Ipsum is simply dummy text of the printing and typesetting industry
             . Lorem Ipsum has been the industry's standard dummyLorem Ipsum is simply 
             dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummyLorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy"/>
+            Lorem Ipsum has been the industry's standard dummy"
+            />
           </div>
-          <div style={{display:'flex',justifyContent:'flex-end',marginTop:'1rem'}}>
-            <Button variant='contained'>Paylaş</Button>
+          <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '1rem' }}>
+            <Button variant="contained">Paylaş</Button>
           </div>
         </div>
       </Modal>
 
-
       {/* Title */}
       <div className={styles.mainTitle}>
         <SettingsTitle text="Clubs" />
-        <Button onClick={() => { setOpen(true) }}>Duyuru Ekle</Button>
+        <Button
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Duyuru Ekle
+        </Button>
       </div>
 
       {/* container */}
-      {/* {myClubs.length ? (
-        <div className={styles.mainContainer}>
-          <Card className={styles.mainCard}>
-            <Card.Img photo={cardImage} />
-            <Card.Body>
-              <Card.Label>In Label</Card.Label>
-              <Card.Title>Çukurov Universitesi</Card.Title>
-              <Card.Owner>
-                <b>By </b>
-                Okçuluk Kulubü
-              </Card.Owner>
-            </Card.Body>
-          </Card>
-        </div>
+      {myClubs.length ? (
+        myClubs.map((club) => (
+          <div className={styles.mainContainer}>
+            <Card className={styles.mainCard}>
+              {/* <Card.Img photo={cardImage} /> */}
+              <Card.Body>
+                <Card.Label>In Label</Card.Label>
+                <Card.Title>{club.ClubName}</Card.Title>
+                <Card.Owner>
+                  <b>By </b>
+                  Okçuluk Kulubü
+                </Card.Owner>
+                <button>Sil</button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))
       ) : (
         <div className={styles.mainNotContainer}>
           <div className={styles.mainNot}>
-            <h3>Üzgünüz! :( Herhangi bir kulübe üye değilsiniz.</h3>
+            <h3>Üzgünüz! :( Herhangi bir klübe sahip değilsiniz.</h3>
           </div>
         </div>
-      )} */}
-      <div className={styles.mainNotContainer}>
-        <div className={styles.mainNot}>
-          <h3>Üzgünüz! :( Herhangi bir kulübe üye değilsiniz.</h3>
-        </div>
-      </div>
+      )}
     </main>
   );
 };
